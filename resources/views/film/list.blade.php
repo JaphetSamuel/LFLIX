04c4c822bfe73a3eb6d1f8d321d8782e124e8c9b
Samuel AGOUA <sagoua1@gmail.com>
2023-12-22 09:24:28 +0000

    add style

--- resources/views/film/list.blade.php
+++ resources/views/film/list.blade.php
@@ -1,11 +1,12 @@
-<html lang="fr">
-    <body>
-        <h1> List  de film {{$nom}}  -
-            @auth
-                <a> ma liste</a>
-            @else
-                <a href="{{ route('user.form') }}"> se connecter </a>
-            @endauth
+@extends('layouts.base')
+
+@section('content')
+    <h1> List  de film {{$nom}}  -
+    @auth
+        <a> ma liste</a>
+    @else
+        <a href="{{ route('user.form') }}"> se connecter </a>
+        @endauth
         </h1>
         <br>
         <br>
@@ -26,18 +27,47 @@
                 <input type="submit" value="Ajouter">
             </fieldset>
         </form>
-    <ul>
-        <h3>Liste de film </h3>
+        <ul class="row">
+            <h3>Liste de film </h3>
             @foreach($films as $film)
-                <li> {{$film->titre}} </li>
+                <div class="col col-md-6 col-lg-4 mt-2">
+                    <div class="card shadow-sm">
+
+                        <img class="card-image" src="https://images.caradisiac.com/logos/2/9/8/7/272987/S8-les-habitants-de-los-angeles-s-opposent-a-fast-furious-197701.jpg" alt="">
+                        <div class="card-body">
+                            <p class="card-text">{{$film->description}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted"> {{$film->votes}} votes</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-    </ul>
+        </ul>
         <br>
-    <ul>
-        <h3>Mes film </h3>
+        <ul class="row">
+            <h3>Mes film </h3>
             @foreach($mesfilms as $film)
-                <li> {{$film->titre}} </li>
+                <div class="col col-md-6 col-lg-4">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">{{$film->titre}}</text></svg>
+
+                        <div class="card-body">
+                            <p class="card-text">{{$film->description}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-    </ul>
-    </body>
-</html>
+        </ul>
+@endsection
